4dee239b662df1205d8026cb0f6f138d1e3fce37
Maxwell Nelson <maxnelson997@gmail.com>
2024-12-29 13:00:11 -0700

    added post creation db

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -8,9 +8,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+    // Grab the data from requests body.
     const { title, body } = req.body;
-    await Post.create({ title, body })
-    res.status(200).json({ msg: 'post request' })
+
+    // Check the fields are not empty.
+    if (!title || !body) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    try {
+        const post = await Post.create({ title, body })
+        res.status(200).json({ success: 'post request', post })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
 });
 
 export { router as postRoutes }
