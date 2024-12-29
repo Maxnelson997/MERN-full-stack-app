c4ed585992576166325004fa30b1b23a199711e0
Maxwell Nelson <maxnelson997@gmail.com>
2024-12-29 13:16:26 -0700

    delete post

---
+++ backend/controllers/postsController.js
@@ -0,0 +1,53 @@
+import Post from '../models/PostModel.js';
+import mongoose from "mongoose";
+
+// ****get all posts**** //
+const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json({ success: 'posts fetched', posts })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+// ****create new post**** //
+const addPost = async (req, res) => {
+    // Grab the data from requests body.
+    const { title, body } = req.body;
+
+    // Check the fields are not empty.
+    if (!title || !body) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    try {
+        const post = await Post.create({ title, body })
+        res.status(200).json({ success: 'post request', post })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+// ****delete post with id**** //
+const deletePost = async (req, res) => {
+    // Check if the ID in the params is valid
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ error: 'Invalid Post ID Param' });
+    }
+
+    // Check if the post exists
+    const post = await Post.findById(req.params.id)
+    if (!post) {
+        return res.status(400).json({ error: 'Post not found' });
+    }
+
+    try {
+        await post.deleteOne()
+        res.status(200).json({ success: 'Post succesfully deleted', post })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export { getPosts, addPost, deletePost };
--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -1,28 +1,13 @@
 import express from 'express';
-import Post from '../models/PostModel.js';
+import { addPost, getPosts, deletePost } from '../controllers/postsController.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ msg: 'Hello User' })
-});
-
-router.post('/', async (req, res) => {
-    // Grab the data from requests body.
-    const { title, body } = req.body;
-
-    // Check the fields are not empty.
-    if (!title || !body) {
-        return res.status(400).json({ error: 'All fields are required.' });
-    }
-
-    try {
-        const post = await Post.create({ title, body })
-        res.status(200).json({ success: 'post request', post })
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-
-});
+// get all posts
+router.get('/', getPosts);
+// add new post
+router.post('/', addPost);
+// delete post
+router.delete('/:id', deletePost);
 
 export { router as postRoutes }
