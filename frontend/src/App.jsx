6c2cebd16f2982f7f8de2be36e896a6e267c7ad5
Maxwell Nelson <maxnelson997@gmail.com>
2025-01-14 20:33:25 -0700

    route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './pages/Layout'
-import Login from './pages/users/Login';
-import Register from './pages/users/Register';
-import Dashboard from './pages/users/Dashboard';
-import Home from './pages/posts/Home';
-import Create from './pages/posts/Create';
-import Update from './pages/posts/Update';
+import Login from './pages/users/Login'
+import Register from './pages/users/Register'
+import Dashboard from './pages/users/Dashboard'
+import Home from './pages/posts/Home'
+import Create from './pages/posts/Create'
+import Update from './pages/posts/Update'
+import AuthRoutes from './Routes/AuthRoutes'
+import GuestRoutes from './Routes/GuestRoutes'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index path="home" element={<Home />} />
+          <Route index element={<Home />} />
 
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="dashboard" element={<Dashboard />} />
-
-          <Route path="create" element={<Create />} />
-          <Route path="update" element={<Update />} />
+          <Route element={<AuthRoutes />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="create" element={<Create />} />
+            <Route path="update" element={<Update />} />
+          </Route>
+          <Route element={<GuestRoutes />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
